f38bfbab208dd499711a94176e5ea0ba5e970c20
ander <anderag062@gmail.com>
2025-05-13 13:22:20 -0600

    Se crearon las carpetas correspondientes para separar los servicios Front del Back

---
+++ Envejecimiento-Exitoso-Backend/src/main/java/com/IngdeSoftware/Equipo2/Envejecimiento/Exitoso/EnvejecimientoExitosoApplication.java
@@ -0,0 +1,13 @@
+package com.IngdeSoftware.Equipo2.Envejecimiento.Exitoso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvejecimientoExitosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvejecimientoExitosoApplication.class, args);
+	}
+
+}
