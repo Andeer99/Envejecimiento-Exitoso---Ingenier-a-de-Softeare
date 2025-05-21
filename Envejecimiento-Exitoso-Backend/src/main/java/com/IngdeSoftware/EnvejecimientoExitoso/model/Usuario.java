9c857fc26a6c280e291d4639c01dcb861684b824
ander <anderag062@gmail.com>
2025-05-21 16:33:16 -0600

    cambios

--- Envejecimiento-Exitoso-Backend/src/main/java/com/IngdeSoftware/EnvejecimientoExitoso/model/Usuario.java
+++ Envejecimiento-Exitoso-Backend/src/main/java/com/IngdeSoftware/EnvejecimientoExitoso/model/Usuario.java
@@ -23,6 +23,46 @@ public class Usuario {
     @Column(nullable = false)
     private String password;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "usuarios_roles",
