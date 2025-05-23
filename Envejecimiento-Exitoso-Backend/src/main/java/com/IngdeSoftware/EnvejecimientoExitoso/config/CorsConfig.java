36fbbcfe3b8b938525589a8761c837e1a3c15b53
ander <anderag062@gmail.com>
2025-05-23 11:33:16 -0600

    cambios a la capa CORS

--- Envejecimiento-Exitoso-Backend/src/main/java/com/IngdeSoftware/EnvejecimientoExitoso/config/CorsConfig.java
+++ Envejecimiento-Exitoso-Backend/src/main/java/com/IngdeSoftware/EnvejecimientoExitoso/config/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "https://envejecimientoexitosofrontend-production.up.railway.app",
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "http://localhost:3000"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
